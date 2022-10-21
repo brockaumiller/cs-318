3fc9e6b4417dceacad42fbd0d49b491229d71bfe
brockaumiller <113037644+brockaumiller@users.noreply.github.com>
2022-10-21 09:48:55 -0500

    labs

--- Lab08/scripts.js
+++ Lab08/scripts.js
@@ -1,5 +1,5 @@
 //Sloth Section
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(addEventListener){
 	document.querySelector('.sloth>.accordion').addEventListener('click', function(){
 		document.querySelector('.sloth>article').classList.toggle('show');
 		document.querySelector('.sloth span').classList.toggle('rotate');
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 //Elephant Section
 document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector('.elephant>.accordion').addEventListener('click', function(){
+	document.querySelector('.elephant>.accordion').addEventListener('click', function(click){
 		document.querySelector('.elephant>article').classList.toggle('show');
 		document.querySelector('.elephant span').classList.toggle('rotate');
 	});
